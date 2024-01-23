162cf37ac5103f77d36eed45e14a864d617579bf
blakedsec <154298337+blakedsec@users.noreply.github.com>
2024-01-22 17:28:12 -0800

    Add files via upload

---
+++ SearchFiles30Days.ps1
@@ -0,0 +1,8 @@
+﻿#Get a list of all folders under "D:\Users"
+Get-ChildItem -Path "D:\Users" -Name | foreach {
+    #If the current folder has a "Scans" directory...
+    if(test-path -path D:\Users\$_\Scans) {
+        #Search all files within the "Scans" directory that are older than 30 days
+        forfiles -p "D:\Users\$($_)\Scans" -s -m *.* -d -30 -c "cmd /c echo @file is at least 30 days old. @PATH"
+    }
+}
